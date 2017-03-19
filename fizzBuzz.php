5f864a82d024db28ddd369c424071f711449193a
gcerni <cerniglia.gianluca@gmail.com>
2017-03-19 17:07:19 +0000

    final version

--- fizzBuzz.php
+++ fizzBuzz.php
@@ -1,25 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>FizzBuzz.php</title>
 </head>
 <body>
-<ul>
-<?php
-
-for ($i = 1; $i <= 100; $i++) {
-	if ($i % 3 == 0 && $i %5 ==0) {
-	echo "<li> {$i} Fizz Buzz</li>"; 
-	} else if ($i % 5 == 0 ) {
-		echo "<li>" . $i . " Buzz</li>";
-	}else if ($i %3 == 0) {
-	echo "<li>" . $i . " Fizz</li>";
-	}
-	
-}
-
-
-?>
-</ul>
+	<ul>
+		<?php
+			for ($i = 1; $i <= 100; $i++) {
+				if ($i % 3 == 0 && $i %5 ==0) {
+					echo "<li> {$i} Fizz Buzz</li>";
+				}
+				else if ($i % 5 == 0 ) {
+					echo "<li>" . $i . " Buzz</li>";
+				}
+				else if ($i %3 == 0) {
+					echo "<li>" . $i . " Fizz</li>";
+				}
+			}
+		?>
+	</ul>
 </body>
 </html>
