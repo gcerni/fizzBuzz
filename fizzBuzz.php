73c31d55fb0d736c89b3b4557e211c48a28b0591
gcerni <cerniglia.gianluca@gmail.com>
2017-03-19 16:59:11 +0000

    first upload

---
+++ fizzBuzz.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<ul>
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+	if ($i % 3 == 0 && $i %5 ==0) {
+	echo "<li> {$i} Fizz Buzz</li>"; 
+	} else if ($i % 5 == 0 ) {
+		echo "<li>" . $i . " Buzz</li>";
+	}else if ($i %3 == 0) {
+	echo "<li>" . $i . " Fizz</li>";
+	}
+	
+}
+
+
+?>
+</ul>
+</body>
+</html>
